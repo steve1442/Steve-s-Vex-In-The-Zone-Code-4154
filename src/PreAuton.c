7722f60b56366a58686096725d6dfc8fa33a18ef
steve1442 <keller1442@gmail.com>
2018-01-18 15:08:47 -0500

    im gonna be honest, i dont know what i did in this version but it compiled

--- src/PreAuton.c
+++ src/PreAuton.c
@@ -1,21 +1,18 @@
 #include "main.h"
+#include <string.h>
+
+int select = -1, menu = 0, menuMax = 3, auton = 1, currLCD = 0, lastLCD = 0;
 
 void runPreAuton()
 {
-  int menu = 0;
-  int menuMax = 3;
-  int auton = 1; //default auton
-  int currLCD = 0;
-  int lastLCD = 0;
-  int select = -1;
-  char *screens1[3] = {"    >Auton 1    ",  "    >Auton 2    ",  "    >Auton 3    "};
+  char *screens1[16][3] = {"    >Auton 1    ",  "    >Auton 2    ",  "    >Auton 3    "};
   char *screens2[3] = {"    20 point    ",  "    Plcehold    ",  "ha i got nothing"};
   while (select == -1)
   {
     currLCD = lcdReadButtons(uart2);
     if (lastLCD == 0 && (currLCD == LCD_BTN_LEFT || currLCD == LCD_BTN_RIGHT)) menu = ( (int)(menu + (currLCD - 2.5) / 1.5) + menuMax) % menuMax;
     if (lastLCD == 0 && currLCD == LCD_BTN_CENTER){ select = menu; auton = menu; }
-    lcdPrint(uart2, 1, screens1[menu]);  lcdPrint(uart2, 2, screens2[menu]);
+    lcdPrint(uart2, 1, screens1[15][menu]);  lcdPrint(uart2, 2, screens2[menu]);
     lastLCD = currLCD;
   }
 }
